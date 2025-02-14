d1ccf543eba1ef0ac95738f0f213324e0cb6b805
Sinan Muhammed <sinan9539391118@gmail.com>
2025-02-14 19:26:01 +0530

    Changed Logo

--- app/src/main/java/com/sinxn/mytasks/ui/screens/TaskListScreen.kt
+++ app/src/main/java/com/sinxn/mytasks/ui/screens/TaskListScreen.kt
@@ -2,7 +2,6 @@ package com.sinxn.mytasks.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
