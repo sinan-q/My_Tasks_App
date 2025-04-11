1fee524f82ae1f2e104a0f4fffc92d4204ee6c77
Sinan Muhammed <sinan9539391118@gmail.com>
2025-04-12 02:31:32 +0530

    Reshedules alarms on restart

--- app/src/main/java/com/sinxn/mytasks/ui/screens/alarmScreen/BootReceiver.kt
+++ app/src/main/java/com/sinxn/mytasks/ui/screens/alarmScreen/BootReceiver.kt
@@ -3,11 +3,34 @@ package com.sinxn.mytasks.ui.screens.alarmScreen
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.sinxn.mytasks.data.repository.AlarmRepository
+import com.sinxn.mytasks.data.repository.TaskRepository
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class BootReceiver : BroadcastReceiver() {
+    @Inject lateinit var alarmScheduler: AlarmScheduler
+    @Inject lateinit var alarmRepository: AlarmRepository
+    @Inject lateinit var taskRepository: TaskRepository
+
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
-            // Reschedule alarms here
+            CoroutineScope(Dispatchers.IO).launch {
+                alarmRepository.getAlarms().forEach { alarm ->
+                    taskRepository.getTaskById(alarm.taskId)?.let { task ->
+                        alarmScheduler.scheduleAlarm(
+                            alarm.alarmId,
+                            task.title,
+                            task.description,
+                            alarm.time
+                        )
+                    }
+                }
+            }
         }
     }
 }
