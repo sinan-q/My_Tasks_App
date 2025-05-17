1981ff628b55ceff20571e893b065fa5416b4937
Sinan Muhammed <sinan9539391118@gmail.com>
2025-05-17 17:16:09 +0530

    Optimize Converters

--- app/src/main/java/com/sinxn/mytasks/utils/Converters.kt
+++ app/src/main/java/com/sinxn/mytasks/utils/Converters.kt
@@ -1,21 +1,19 @@
 package com.sinxn.mytasks.utils
 
 import androidx.room.TypeConverter
-import java.time.Instant
 import java.time.LocalDateTime
-import java.time.ZoneId
 
 class Converters {
 
     @TypeConverter
     fun epochMilliToLocalDate(value: Long?): LocalDateTime? {
         return value?.let {
-            if(it==0L) null else LocalDateTime.ofInstant(Instant.ofEpochMilli(it), ZoneId.systemDefault())
+            if(it==0L) null else fromMillis(it)
         }
     }
 
     @TypeConverter
     fun localDateToEpochMilli(date: LocalDateTime?): Long {
-        return date?.atZone(ZoneId.systemDefault())?.toInstant()?.toEpochMilli()?:0L
+        return date?.toMillis()?:0L
     }
 }
